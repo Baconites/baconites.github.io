cf0efbfeba6e9091c634fa655b752fc6e5f2385e
unknown <teerayuth.a@ku.th>
2019-06-02 01:00:51 +0700

    add newroomteacher

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+
+	$('#add').click(function(){
+		var show = $('#problem').html();
+		var x = '<div class="form-group"><label for="question">Enter your question : </label><input type="text" class="form-control" placeholder="question"><label for="question">Answer : </label>'
+		x+='<input type="text" class="form-control" placeholder="answer 1">"<input type="text" class="form-control" placeholder="answer 2">'
+		x+='<div class="form-inline"><label for="realanswer">Right answer is : </label><select name="" id=""><option value="1">1</option>'
+		x+='<option value="2">2</option></select></div></div>';
+		$('#problem').html(show+x);
+
+	});
+
 $(window).on('load', function() {
-	/*------------------
-		Preloder
-	--------------------*/
 	$(".loader").fadeOut(); 
 	$("#preloder").delay(400).fadeOut("slow");
-
 });
